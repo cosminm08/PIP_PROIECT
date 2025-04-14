8831275ce48a29235cae8c10e86f453a5ab737ac
MirceaH <mircea-nicolae.hosofschi@student.tuiasi.ro>
2025-04-14 08:12:34 +0300

    Add files via upload

---
+++ PROIECT_TEST/PIP_PROIECT-main/Proiect_PIP/src/smart_house/Proximitate.java
@@ -0,0 +1,75 @@
+package smart_house;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+class proxi extends JFrame{
+	private static final long serialVersionUID = 1L;
+	proxi(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Senzor ultrasonic");
+		setSize(800, 410);
+		
+		setLayout(null);
+		
+		JButton b1 = new JButton("Close");
+		b1.setBounds(700, 15, 80, 20);
+		b1.addActionListener(e->dispose());
+		
+		JButton b2 = new JButton("Prev.");
+		b2.setBounds(620, 15, 80, 20);
+		b2.addActionListener(e->{
+			dispose();
+			new control();
+		});
+		
+		JButton b3 = new JButton("OFF");
+		b3.setBounds(15, 15, 80, 20);
+		b3.setBackground(Color.RED);
+	    b3.setForeground(Color.WHITE);
+	    b3.setFont(new Font("Segoe UI", Font.BOLD, 16));
+	    b3.setFocusPainted(false);
+	    
+	    
+		
+	    b3.addActionListener(new ActionListener() {
+            private boolean isOn = false;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                isOn = !isOn;
+                if (isOn) {
+                    b3.setText("ON");
+                    b3.setBackground(new Color(0, 180, 0));
+                } else {
+                    b3.setText("OFF");
+                    b3.setBackground(Color.RED);
+                }
+            }
+        });
+		
+		JTextArea area = new JTextArea();
+		area.setBounds(30, 60, 400, 280);
+		
+		add(b1);
+		add(b2);
+		add(b3);
+		add(area);
+		
+		setVisible(true);
+	}
+}
+
+public class Proximitate {
+
+	public static void main(String[] args) {
+		new proxi();
+	}
+
+}
